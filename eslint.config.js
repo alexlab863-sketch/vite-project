bfc691ec5c2969a448d0c780edd1d1a0a1ce3b30
Firdavs <f27629462@gmailcom>
2026-02-05 21:46:26 +0500

    2 ta page qoshdim

---
+++ eslint.config.js
@@ -0,0 +1,23 @@
+import js from "@eslint/js";
+import globals from "globals";
+import pluginReact from "eslint-plugin-react";
+import { defineConfig } from "eslint/config";
+
+export default defineConfig([
+  {
+    files: ["**/*.{js,mjs,cjs,jsx}"],
+    plugins: { js },
+    extends: ["js/recommended"],
+    languageOptions: { globals: globals.browser },
+    rules: {
+      "react/react-in-jsx-scope": "off",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  pluginReact.configs.flat.recommended,
+]);
---
+++ src/About.jsx
@@ -0,0 +1,3 @@
+export default function About() {
+    return <h2>Hi this is from page2</h2>
+}
