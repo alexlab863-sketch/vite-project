4b2e8f98d645efeee1bf25f2ea23f6573b57169f
Firdavs <f27629462@gmailcom>
2026-02-06 12:12:40 +0500

    Forma ishlari butunlay yakunlandi

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,26 +1,43 @@
-import {Link} from "react-router-dom"
-export default function Home() {
+import { Link } from "react-router-dom";
+import "./Home.css"
 
-  function handleform(e){
-e.preventDefault()
+export default function Home() {
+  function handleForm(e) {
+    e.preventDefault();
+
+    const email = e.target.email.value.trim();
+    const password = e.target.password.value.trim();
+
+    if (!email || !password) {
+      alert("Iltimos, barcha maydonlarni toldiring!");
+      return;
+    }
+
+    alert("Muvaffaqiyatli yuborildi ");
   }
+
   return (
     <>
       <h1>Home</h1>
       <Link to="/about">About</Link>
-       <form onSubmit={handleform} className="forma">
-        <p>
-          <input type="text" id="name"/>
-          <label htmlFor="name">ism kiriting</label>
-        </p>
 
-        <p>
-          <input type="password" id="password"/>
-          <label htmlFor="password">parol  kiriting</label>
-        </p>
+      <form onSubmit={handleForm} className="login-form">
+        <h2 className="login-title">Login</h2>
 
-        <button type="submit">submit</button>
-       </form>
+        <div className="form-group">
+          <input type="email" name="email" id="email" />
+          <label htmlFor="email">Email kiriting</label>
+        </div>
+
+        <div className="form-group">
+          <input type="password" name="password" id="password" />
+          <label htmlFor="password">Parol kiriting</label>
+        </div>
+
+        <button className="submit-btn" type="submit">
+          Submit
+        </button>
+      </form>
     </>
-  )
+  );
 }
