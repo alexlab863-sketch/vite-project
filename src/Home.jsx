15ad09579270b31740d59049966e872c89011325
Firdavs <f27629462@gmailcom>
2026-02-06 11:36:43 +0500

    Forma qo'shdim

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,9 +1,26 @@
 import {Link} from "react-router-dom"
 export default function Home() {
+
+  function handleform(e){
+e.preventDefault()
+  }
   return (
     <>
       <h1>Home</h1>
       <Link to="/about">About</Link>
+       <form onSubmit={handleform} className="forma">
+        <p>
+          <input type="text" id="name"/>
+          <label htmlFor="name">ism kiriting</label>
+        </p>
+
+        <p>
+          <input type="password" id="password"/>
+          <label htmlFor="password">parol  kiriting</label>
+        </p>
+
+        <button type="submit">submit</button>
+       </form>
     </>
   )
 }
